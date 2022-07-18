3ce6d95423b85e9cfe8d44e2414325ef170a0fba
byyy <brantyang234@gmail.com>
2022-07-17 22:53:06 -0400

    save

---
+++ main.py
@@ -0,0 +1,42 @@
+class Person():
+  def __init__(self, firstName, lastName):
+    self.firstName = firstName
+    self.lastName = lastName
+  bday = 24
+  # bday = input("Your birthday is: ")
+
+  def __repr__(self):
+    return(self.firstName + ' ' + self.lastName)
+
+  def getLast(self):
+    return(self.lastName)
+
+  def getbday(self):
+    return(self.bday)
+
+  def __lt__(self, other):
+    return self.lastName < other.lastName
+  
+
+class schoolPerson(Person):
+  schoolPerson.id = 0
+  def __init__(self, firstName, lastName):
+    super().__init__(firstName, lastName)
+    self.ID = schoolPerson.id
+    schoolPerson.id = schoolPerson.id + 1
+    
+  def __repr__(self):
+    return(self.firstName + ' ' + self.lastName + ' ' + str(self.ID))
+
+  def getID (self):
+    return(self.ID)
+  
+  def __lt__(self, other):
+    return self.ID < other.ID
+
+p1 = schoolPerson("Brant", "Yang")
+p2 = schoolPerson("Tait", "Duan")
+p3 = schoolPerson("David", "Dong")
+
+people = [p1,p2,p3]
+print(sorted(people))
