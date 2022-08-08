4db450ee0f0404b75cc0c811ef8f4d7572e9b79a
byyy <brantyang234@gmail.com>
2022-08-07 22:10:24 -0400

    fixed the bug, but not getting the number

--- main.py
+++ main.py
@@ -6,25 +6,29 @@ beta = 0.574
 def F (x, y, z, b):
 	sum = 0
 	for m in range(1,1000):
-		n1 = math.exp((b**2)*(m**2)*(x-z)) - math.exp(-(b**2)*(m**2)*(x-y))
-		d1 = (b**2)*(m**2)
-		sigma = n1/d1
+		n1 = round(math.exp(-(b**2)*(m**2)*(x-z)) , 4)
+		#print((b**2)*(m**2)*(x-z))
+		n2 = round(math.exp(-(b**2)*(m**2)*(x-y)) , 4)
+		d1 = round((b**2)*(m**2) , 4)
+		sigma = round((n1 - n2)/d1 , 3)
 		sum = sum + sigma
+		# print(sum)
 
 	return (z - y + 2 * sum)
 
 def alpha():
 	sum = 0
-	for k in range(0, failTaskNO):
+	for k in range(1, failTaskNO):
 		sigma = current[k] * F(failTime, startTime[k], startTime[k] + duration[k], beta)
 		sum = sum + sigma
 
-	return sum + currentU * F(failTime, failTaskStartTime, failTime, beta)
+
+	return round(sum, 3) + currentU * F(failTime, failTaskStartTime, failTime, beta)
 
 current = [912, 912]
 startTime = [0, 35]
-duration = [25, 25]
-failTime = 43.8
+duration = [25, 9.2]
+failTime = 44.2
 failTaskNO = 2
 failTaskStartTime = 35
 currentU = 912
