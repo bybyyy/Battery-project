31121729b5c9472bdaf8f7d1cc45f041b7ff154c
byyy <brantyang234@gmail.com>
2022-08-11 16:08:50 -0400

    fixed the bug, but not getting the number

--- main.py
+++ main.py
@@ -2,27 +2,51 @@ import math
 
 #beta = pi*sqrt(diffusionCoefficient)/length #0.574
 beta = 0.574
+unused = 0
 
 def F (x, y, z, b):
 	sum = 0
-	for m in range(1,1000):
-		n1 = round(math.exp(-(b**2)*(m**2)*(x-z)) , 4)
+	for m in range(1,10):
+		n1 = math.exp(-(b**2)*(m**2)*(x-z))
 		#print((b**2)*(m**2)*(x-z))
-		n2 = round(math.exp(-(b**2)*(m**2)*(x-y)) , 4)
-		d1 = round((b**2)*(m**2) , 4)
+		n2 = math.exp(-(b**2)*(m**2)*(x-y))
+		d1 = (b**2)*(m**2)
 		sigma = round((n1 - n2)/d1 , 3)
 		sum = sum + sigma
-		# print(sum)
+	unused = sum
 
-	return (z - y + 2 * sum)
+	return z - y + 2* sum
 
-def alpha():
+def Capacity():
 	sum = 0
 	for k in range(0, failTaskNO):
 		sigma = current[k] * F(failTime, startTime[k], startTime[k] + duration[k], beta)
 		sum = sum + sigma
 
-	return round(sum, 3) + currentU * F(failTime, failTaskStartTime, failTime, beta)
+	return sum
+	# return round(sum, 3) + currentU * F(failTime, failTaskStartTime, failTime, beta)
+
+def wasted (x, y, z, b):
+	unused = 0
+	for m in range(1,10):
+		n1 = math.exp(-(b**2)*(m**2)*(x-z))
+		n2 = math.exp(-(b**2)*(m**2)*(x-y))
+		d1 = (b**2)*(m**2)
+		sigma = round((n1 - n2)/d1 , 3)
+		unused = unused + sigma
+
+	return unused
+
+def totalUnsed():
+	sum = 0
+	for k in range(0, failTaskNO):
+		sigma = current[k] * wasted(failTime, startTime[k], startTime[k] + duration[k], beta)
+		sum = sum + sigma
+
+	return sum
+
+def totalCharge(total, wasted):
+	return total - wasted
 
 current = [912, 912]
 startTime = [0, 35]
@@ -32,4 +56,7 @@ failTaskNO = 2
 failTaskStartTime = 35
 currentU = 912
 
-print(alpha())
+
+print("total,", Capacity())
+print("unused,", totalUnsed())
+print("used," , totalCharge(Capacity(), totalUnsed()))
