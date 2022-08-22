3c4d6530b749507e5d0c38c3e098d9f485833665
byyy <brantyang234@gmail.com>
2022-08-21 22:14:44 -0400

    able to find the task when the battery fails at

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 import random
 
-alpha = 120000
+alpha = 200000
 # beta = pi*sqrt(diffusionCoefficient)/length #0.574
 beta = 0.574
 unused = 0
@@ -22,26 +22,15 @@ class task:
     def __lt__(self, other):
         return self.amperage > other.amperage
 
-
-for x in range(1, NumberofTasks + 1):  # random generate certain amount of tasks
-    amperage = random.randint(10, 900)
-    duration = random.randint(1, 5)  # minutes
-    tasks.append(task(x, 0, amperage, duration))
-
-tasks = sorted(tasks)  # arrange them in descending order of their amperage
-for x in range(1, NumberofTasks + 1):  # recalculate the start time for all tasks in the new arrangement
-    if x == 1:
-        startTime = 0
-    else:
-        startTime = tasks[x - 2].startTime + tasks[x - 2].duration
-    tasks[x - 1].startTime = startTime
+    # def __lt__(self, other):
+    #     return self.amperage < other.amperage
 
 
-for obj in tasks:
-    print(obj.ID, obj.startTime, obj.amperage, obj.duration)
-
-endTime = tasks[len(tasks) - 1].startTime + tasks[len(tasks) - 1].duration
-failTaskNO = len(tasks)
+def printall(total, wasted, consume):
+    print("total,", total)
+    print("unused,", wasted)
+    print("used,", consume)
+    print("")
 
 
 def swap(arr, a, b):
@@ -76,10 +65,6 @@ def Qsort(array):  # sort the array in descending order
     return [*larray, pivot, *rarray]
 
 
-# example = [2, 5, 6, 1, 4, 6, 2, 4, 7, 8]
-# print(Qsort(example))
-
-
 def F(x, y, z, b):
     sum = 0
     for m in range(1, 10):
@@ -92,14 +77,14 @@ def F(x, y, z, b):
     return z - y + 2 * sum
 
 
-def Capacity():
+def Capacity(arr, endtime, failtaskindex):
     sum = 0
-    for k in range(0, failTaskNO):
-        sigma = tasks[k].amperage * F(endTime, tasks[k].startTime, tasks[k].startTime + tasks[k].duration, beta)
+    for k in range(0, failtaskindex + 1):
+        sigma = arr[k].amperage * F(endtime, arr[k].startTime, arr[k].startTime + arr[k].duration, beta)
         sum = sum + sigma
 
     # return round(sum, 3) + currentU * F(failTime, failTaskStartTime, failTime, beta)
-    return sum
+    return round(sum, 3)
 
 
 def wasted(x, y, z, b):
@@ -118,10 +103,10 @@ def wasted(x, y, z, b):
     return 2 * unused
 
 
-def totalUnsed():
+def totalUnsed(arr, endtime, failtaskindex):
     sum = 0
-    for k in range(0, failTaskNO):
-        sigma = tasks[k].amperage * wasted(endTime, tasks[k].startTime, tasks[k].startTime + tasks[k].duration, beta)
+    for k in range(0, failtaskindex + 1):
+        sigma = arr[k].amperage * wasted(endtime, arr[k].startTime, arr[k].startTime + arr[k].duration, beta)
         sum = sum + sigma
 
     return round(sum, 3)
@@ -131,16 +116,92 @@ def totalCharge(total, wasted):
     return total - wasted
 
 
-print("total,", Capacity())
-print("unused,", totalUnsed())
-print("used,", totalCharge(Capacity(), totalUnsed()))
+def searchfailtaskindex(tasks, a):
+    index = 0
+    for i in range(0, len(tasks)):
+        tarrary = []
+        for j in range(0, i + 1):
+            tarrary.append(tasks[j])
+        if Capacity(tarrary, tarrary[len(tarrary) - 1].startTime + tarrary[len(tarrary) - 1].duration, i) < a:
+            index += 1
+        else:
+            return index + 1
+    return -1
+
+
+def binarySearch(arr, i, r, x):
+    if r >= i:
+        m = i + (r - i) // 2
+
+        tendtime = arr[m].startTime + arr[m].duration
+        tarr = splitarray(arr, 0, m)
+        narr = splitarray(arr, 0, m + 1)
+
+        if Capacity(tarr, tendtime, m) < x and Capacity(narr, tendtime, m + 1) > x:
+            return m
+        elif Capacity(tarr, tendtime, m) > x:
+            return binarySearch(arr, i, m - 1, x)
+        else:
+            return binarySearch(arr, m + 1, r, x)
+
+    else:
+        return -1
+
+
+def splitarray(arr, i, f):
+    newarr = []
+
+    for a in range(i, f + 1):
+        newarr.append(arr[a])
+    return newarr
+
+
+for x in range(1, NumberofTasks + 1):  # random generate certain amount of tasks
+    amperage = random.randint(10, 900)
+    duration = random.randint(1, 10)  # minutes
+    tasks.append(task(x, 0, amperage, duration))
 
 # tasks = sorted(tasks)  # arrange them in descending order of their amperage
-# for x in range(1, NumberofTasks + 1):  # recalculate the start time for all tasks in the new arrangement
-#     if x == 1:
-#         startTime = 0
-#     else:
-#         startTime = tasks[x - 2].startTime + tasks[x - 2].duration
-#     tasks[x - 1].startTime = startTime
-#
-# print("total,", Capacity())
+for x in range(1, NumberofTasks + 1):  # recalculate the start time for all tasks in the new arrangement
+    if x == 1:
+        startTime = 0
+    else:
+        startTime = tasks[x - 2].startTime + tasks[x - 2].duration
+    tasks[x - 1].startTime = startTime
+
+# tasks = []
+# tasks.append(task(1, 0, 912, 25))
+# tasks.append(task(2, 35, 912, 9.2))
+
+for obj in tasks:
+    print(obj.ID, obj.startTime, obj.amperage, obj.duration)
+
+endTime = tasks[len(tasks) - 1].startTime + tasks[len(tasks) - 1].duration
+failTaskIn = len(tasks) - 1
+
+capacity = Capacity(tasks, endTime, failTaskIn)
+orgunused = totalUnsed(tasks, endTime, failTaskIn)
+consumption = totalCharge(capacity, orgunused)
+print("\nbefore sorted")
+printall(capacity, orgunused, consumption)
+
+tasks = sorted(tasks)  # arrange them in descending order of their amperage
+for x in range(1, NumberofTasks + 1):  # recalculate the start time for all tasks in the new arrangement
+    if x == 1:
+        startTime = 0
+    else:
+        startTime = tasks[x - 2].startTime + tasks[x - 2].duration
+    tasks[x - 1].startTime = startTime
+
+sortCapacity = Capacity(tasks, endTime, failTaskIn)
+sortUnused = totalUnsed(tasks, endTime, failTaskIn)
+sortConsumption = totalCharge(sortCapacity, sortUnused)
+
+print("after sorted")
+printall(sortCapacity, sortUnused, sortConsumption)
+
+print("alpha - energy consumption = ", round(alpha - sortCapacity, 3))
+
+print(searchfailtaskindex(tasks, alpha) + 1)
+
+# print(binarySearch(tasks, 0, len(tasks) - 1, alpha))
